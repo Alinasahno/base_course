bc112ec6566625124913dc414370f4b30d80b96d
Alinasahno <asahno2009@mail.ru>
2025-11-13 16:49:43 +0000

    ЛАБА2

---
+++ task_2.py
@@ -0,0 +1,6 @@
+import numpy as np
+import task_1 as cnst
+h = 100
+a = np.radians(45)
+b = np.radians(35)
+v = np.sqrt(cnst.ACCELIRATION_OF_GRAVITY*)
---
+++ task_3.py
@@ -0,0 +1,3 @@
+import numpy as np
+import task_1 as t1
+x_0 int(input("Введите значение x0 :"))
